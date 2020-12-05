91beec0ea2e7467e58d916526df72f8b225ad61a
hermanda <daniel.herman19@gmail.com>
2020-12-05 16:08:35 +0100

    day05

---
+++ day05_1.jl
@@ -0,0 +1,26 @@
+
+seats = []
+open("day05_1.in") do io
+    global map_str, passports
+    for line in readlines(io)
+        push!(seats, line)
+    end
+end
+
+function seatID(s::String)::Int64
+    id = 0
+    for chi in 1:10
+        id *= 2
+        if s[chi] == 'B' || s[chi] == 'R'
+            id += 1
+        end
+    end
+    return id
+end
+
+seatID_max = 0
+for seat_s in seats
+    global seatID_max
+    seatID_max = max(seatID_max, seatID(seat_s))
+end
+println(seatID_max)
---
+++ day05_2.jl
@@ -0,0 +1,33 @@
+
+seats_check = zeros(Bool, 2^10)
+
+seats = []
+open("day05_1.in") do io
+    global map_str, passports
+    for line in readlines(io)
+        push!(seats, line)
+    end
+end
+
+function seatID(s::String)::Int64
+    id = 0
+    for chi in 1:10
+        id *= 2
+        if s[chi] == 'B' || s[chi] == 'R'
+            id += 1
+        end
+    end
+    return id
+end
+
+for seat_s in seats
+    global seats_check
+    seats_check[seatID(seat_s)+1] = true
+end
+
+for seat in 100:800
+    global seats_check
+    if !seats_check[seat]
+        println(seat-1)
+    end
+end
